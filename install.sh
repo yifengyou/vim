526906e404f5b8549eda1a2bf5bec394d7139b2b
yifengyou <842056007@qq.com>
2022-03-09 23:29:44 +0800

    modify install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 
-if ! cat ~/.vimrc | grep "github.com/yifengyou/bash" > /dev/null ; then
-	cp vimrc ~/.vimrc -a
-	echo "Install vimrc Success!"
-else
-	echo "Already installed vimrc!"
-fi
+cp vimrc ~/.vimrc -a
+echo "Install vimrc Success!"
+
+mkdir -p ~/.vim/autoload/ &> /dev/null || true
+cp plug.vim ~/.vim/autoload/plug.vim -a
+echo "Install plug.vim Success!"
 
 echo "wait a moment..."
+sleep 1
 vim -c "PlugInstall" -c "q" -c "q" 
 echo "All done! Enjoy ~~ " 
 
